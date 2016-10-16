8175f293925d275d9d28cbd7462ee6e859460c97
yizhi <zhangyiizhi@126.com>
2016-10-16 23:25:45 +0800

    fix

--- 1/clustering_hs300.py
+++ 1/clustering_hs300.py
@@ -2,21 +2,53 @@
 #对沪深三百股票进行聚类，并画出关系图
 import tushare as ts
 import sqlalchemy as create_engine
-
 import MySQLdb as mdb
+import datetime
 
 
 
 
 #get hs300's names
-hs300 = ts.get_hs300s()
-for i in range(len(hs300)):
-	tHs = {};
-	tHs = hs300.ix[i,['name','code']];
-	print tHs['name'],tHs['code'];
+
+def get_hs300():
+	db_host = 'localhost'
+	db_user = 'root'
+	db_password = ''
+	db_name = 'securities_master'
+	con = mdb.connect(host=db_host, user=db_user, passwd=db_password, db=db_name)
+	now = datetime.datetime.utcnow()
+
+	hs300 = ts.get_hs300s()
+	column_str = """ticker, instrument, name, sector, currency, created_date, last_updated_date"""
+	insert_str = ("%s, " * 7)[:-2]
+	final_str = "INSERT INTO symbol (%s) VALUES (%s)" % (column_str, insert_str)
+	symbols = []
+
+	for i in range(len(hs300)):
+		t = hs300.ix[i]
+		symbols.append(
+			(
+				t['code'],
+				'stock',
+				t['name'],
+				'',
+				'RMB',
+				now,
+				now,
+				)
+			)
+	cur = con.cursor()
+	with con: 
+		cur = con.cursor()
+		cur.executemany(final_str, symbols)
+	print 'success insert hs300 into symbol!'
+		
+
 
 	#get every detail data
-	tData = ts.get_hist_data(tHs['code'],start='2016-01-01',end='2016-10-15')
-	print tData
+	# tData = ts.get_hist_data(tHs['code'],start='2016-01-01',end='2016-10-15')
+	# print tData
 
+if __name__ == '__main__':
+	get_hs300();
 
