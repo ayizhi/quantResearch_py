dc40ba3081b126ad77dd6883cb3c84a46b146023
张奕智 <zhangyiizhi@126.com>
2016-10-30 14:16:04 +0800

    fix

--- 2/FeatureUtils.py
+++ 2/FeatureUtils.py
@@ -9,7 +9,7 @@ import tushare as ts
 def CCI(data,ndays):
 	TP = (data['high'] + data['low'] + data['close'])/3 
 	CCI = pd.Series((TP - pd.rolling_mean(TP, ndays)) / (0.015 * pd.rolling_std(TP, ndays)),name='CCI')
-	data.join(CCI)
+	data = data.join(CCI)
 	return data
 
 #timeLag
@@ -20,7 +20,7 @@ def TL(data,ndays):
 	pO = data['open'] - data['open'].shift(1)
 	timeLag = pO/(pH - pL)
 	timeLag.name = 'TL'
-	data.join(timeLag)
+	data = data.join(timeLag)
 	return data
 
 
@@ -30,20 +30,20 @@ def EVM(data,ndays):
 	br = (data['volume']/100000000)/((data['high'] - data['low']))
 	EVM = dm/br
 	EVM_MA = pd.Series(pd.rolling_mean(EVM,ndays),name='EVM')
-	data.join(EVM_MA)
+	data = data.join(EVM_MA)
 	return data
 
 # Simple Moving Average 
 def SMA(data, ndays): 
 	SMA = pd.Series(pd.rolling_mean(data['close'], ndays), name = 'SMA') 
-	data.join(SMA)
+	data = data.join(SMA)
 	return data
 
 # Exponentially-weighted Moving Average 
 def EWMA(data, ndays): 
 	EMA = pd.Series(pd.ewma(data['close'], span = ndays, min_periods = ndays - 1), 
 	name = 'EWMA_' + str(ndays))  
-	data.join(EMA)
+	data = data.join(EMA)
 	return data
 
 
@@ -52,13 +52,13 @@ def ROC(data,n):
 	N = data['close'].diff(n)
 	D = data['close'].shift(n)
 	ROC = pd.Series(N/D,name='Rate of Change')
-	data.join(ROC)
+	data = data.join(ROC)
 	return data 
 
 # Force Index 
 def ForceIndex(data, ndays): 
 	FI = pd.Series(data['close'].diff(ndays) * data['volume'], name = 'ForceIndex') 
-	data.join(FI)
+	data = data.join(FI)
 	return data
 
 # Compute the Bollinger Bands 
@@ -69,7 +69,7 @@ def BBANDS(data, ndays):
 	B1 = pd.Series(b1, name = 'Upper BollingerBand') 
 	b2 = MA - (2 * SD)
 	B2 = pd.Series(b2, name = 'Lower BollingerBand') 
- 	data.join([B1,B2])
+ 	data = data.join([B1,B2])
  	return data
 
 
--- 2/find_features.py
+++ 2/find_features.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 import pandas as pd
+import numpy as np
 from pandas import DataFrame,Series
 import matplotlib.pyplot as plt
 import tushare as ts
@@ -10,7 +11,7 @@ from sklearn.ensemble import ExtraTreesClassifier
 
 
 #获取沪深300股指数据
-hs300 = ts.get_hist_data('hs300')
+hs300 = DataFrame(ts.get_hist_data('hs300'))
 hs300 = toDatetime(hs300)
 
 #加入feature
@@ -22,11 +23,46 @@ hs300 = FeatureUtils.EWMA(hs300,10)
 hs300 = FeatureUtils.ROC(hs300,10)
 hs300 = FeatureUtils.ForceIndex(hs300,10)
 hs300 = FeatureUtils.BBANDS(hs300,10)
+hs300 = hs300.dropna()
 
-print hs300.head(10)
+#归一化
+hs300_norm = (hs300 - hs300.mean())/(hs300.max() - hs300.min())
+
+# # Build a classification task using 3 informative features
+X = hs300_norm.drop('close',1)
+y = np.asarray(hs300_norm['close'], dtype="|S6")
+x_columns = X.columns
+
+
+# Build a forest and compute the feature importances
+forest = ExtraTreesClassifier(n_estimators=10000,
+                              random_state=0)
+forest.fit(X, y)
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+indices = np.argsort(importances)[::-1]
+
+
+# Print the feature ranking
+print("Feature ranking:")
+
+for f in range(X.shape[1]):
+	print f,indices[f],x_columns[int(indices[f])],'===========', importances[indices[f]]
+	# print("%d. feature %d (%f)" % (f + 1,indices[f], importances[indices[f]]))
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(X.shape[1]), importances[indices],
+       color="r", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), indices)
+plt.xlim([-1, X.shape[1]])
+plt.show()
 
 
 
-print hs300.index,hs300.columns
+
+
 
 
