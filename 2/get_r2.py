2e570a975fe551327a2c9a6ab371023628d6e6ca
张奕智 <zhangyiizhi@126.com>
2016-11-04 20:23:34 +0800

    fix

--- 2/get_r2.py
+++ 2/get_r2.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import pprint
 
 import os
 path = os.getcwd()
 print path
 
-data = pd.read_csv(path + '/2/HFT_XY_unselected.csv',encoding='gbk')
-print data
+data = pd.read_csv(path + '/2/hs.csv',parse_dates=True)
+data = pd.DataFrame(data)
+print data.shape
