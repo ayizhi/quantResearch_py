effbcf7ac6193eca66d86b4dc8d344ee01768986
张奕智 <zhangyiizhi@126.com>
2017-01-03 22:28:35 +0800

    fix

--- back_test_system/self/mac.py
+++ back_test_system/self/mac.py
@@ -12,8 +12,42 @@ class MovingAverageCrossStrategy(Strategy):
 	def __init__(self,bars,events,short_window=100,long_window=400):
 		self.bars = bars
 		self.symbol_list = self.bars.symbol_list
-		self.evnets = events
+		self.events = events
 		self.short_window = short_window
 		self.long_window = long_window
 
 		self.bought = self._calculate_initial_bought()
+
+	def _calculate_initial_bought(self):
+		bought = {}
+		for s in self.symbol_list:
+			bought[s] = 'OUT'
+		return bought
+
+	def calculate_signals(self,event):
+		if event.type == 'MARKET':
+			for symbol in self.symbol_list:
+				bars = self.bars.get_latest_bars_values(symbol,'close',N=self.long_window)
+				if bars is not None and bars != []:
+					short_sma = np.mean(bars[-self.short_window:])
+					long_sma = np.mean(bars[-self.long_window:])
+
+					dt = self.bars.get_latest_bar_datetime(symbol)
+					sig_dir = ''
+					strength = 1.0
+					strategy_id = 1
+
+					if short_sma > long_sma and self.bought[symbol] == 'OUT':
+						sig_dir = 'LONG'
+						signal = SignalEvent(strategy_id,symbol,dt,sig_dir,strength)
+						self.events.put(signal)
+						self.bought[symbol] = 'LONG'
+					elif short_sma < long_sma and self.bought[symbol] == 'LONG':
+						sig_dir = 'EXIT'
+						signal = SignalEvent(
+							strategy_id,symbol,dt,sig_dir,strength)
+						self.events.put(signal)
+						self.bought[symbol] = 'OUT'
+
+
+
