ee3b0b12330f8b52c2634c50e20ac4f1c1f43353
张奕智 <zhangyiizhi@126.com>
2017-02-06 11:38:12 +0800

    fux

--- strategy_1/db.py
+++ strategy_1/db.py
@@ -3,6 +3,41 @@ import tushare as ts
 import MySQLdb as mdb
 import datetime
 import time
+import sqlite3
+
+con = sqlite3.connect('../ticker_data/securities_master.db')
+
+def save_hs300_into_db():
+	db_host = 'localhost'
+	db_user = 'root'
+	db_password = ''
+	db_name = 'securities_master'
+	con = mdb.connect(host=db_host, user=db_user, passwd=db_password, db=db_name)
+	now = datetime.datetime.utcnow()
+	hs300 = ts.get_hs300s()
+	column_str = """ticker, instrument, name, sector, currency, created_date, last_updated_date"""
+	insert_str = ("%s, " * 7)[:-2]
+	final_str = "INSERT INTO symbol (%s) VALUES (%s)" % (column_str, insert_str)
+	symbols = []
+
+	for i in range(len(hs300)):
+		t = hs300.ix[i]
+		symbols.append(
+			(
+				t['code'],
+				'stock',
+				t['name'],
+				'',
+				'RMB',
+				now,
+				now,
+				)
+			)
+	cur = con.cursor()
+	with con:
+		cur = con.cursor()
+		cur.executemany(final_str, symbols)
+	print 'success insert hs300 into symbol!'
 
 #获取事先储存过的300支股票
 def get_hs300_tickers():
