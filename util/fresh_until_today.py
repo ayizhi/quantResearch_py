48c1fa46822a02b51d1611ba4cbc2144cc94bd45
张奕智 <zhangyiizhi@126.com>
2017-02-08 19:03:28 +0800

    fix

--- util/fresh_until_today.py
+++ util/fresh_until_today.py
@@ -13,18 +13,25 @@ if __name__ == '__main__':
 		ticker_name = ticker[2]
 		vendor_id = i;
 
+		print '--------------------- %s ---------------------' % vendor_id
+
 		try:
-			start_date = str(start_date[0][0] + datetime.timedelta(days = 1))[0:10]
-			ticker_data = get_ticker_info_by_id(ticker_id,start_date)
+			start_date = get_last_date(ticker_id)[0]
+			start_date = str(start_date[0] + datetime.timedelta(days = 1))[0:10]
 		except:
-			ticker_data = get_ticker_info_by_id(ticker_id,'')
+			start_date = ''
 
 
+		ticker_data = get_ticker_info_by_id(ticker_id,start_date)
+
+		print ('data_shape:' , ticker_data.shape)
+
+		if ticker_data.shape[0] != 0:
+			#存储
+			try:
+				save_ticker_into_db(ticker_id,ticker_data,vendor_id)
+			except:
+				print '数据有问题！'
 
 
-		print (ticker_data)
-
-		#存储
-		# if ticker_data != None:
-		# 	save_ticker_into_db(ticker_id,ticker_data,vendor_id)
 
